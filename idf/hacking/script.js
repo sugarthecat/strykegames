5e6db297652c45216033cf842be09ec18ec8ff94
TJ Nickerson <TJPNick@gmail.com>
2023-05-21 17:59:33 -0400

    lowered command count

--- idf/hacking/script.js
+++ idf/hacking/script.js
@@ -23,11 +23,18 @@ let sysarr = [];
 let awaitedMessage = "";
 
 const startingPrompt = "IDF MILITARY INTELLIGENCE CONSOLE - LOADING . . . 1ESTABLISHING SECURE CONNECTION . . .1VERIFYING CREDENTIALS . . .1PREPARING CONSOLE . . .1<h2>Israeli Defense Forces</h2>ENTER COMMAND: "
-const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
+
 const finalprompt = startingPrompt.replaceAll(1, "<br/>")
 let currentCMDprompt = "";
 let chars = 0
 
+let wordbank = [];
+async function loadWordBank(){
+    let text = fetch("words.txt").then(x => x.text())
+    wordbank = (await text).split("\r\n");
+    console.log(wordbank)
+}
+loadWordBank();
 async function pasteContent() {
     try {
         let text = await navigator.clipboard.readText();
@@ -58,77 +65,41 @@ function enterCommand(cmd) {
 
     if (formattedCommand == "help") {
         currentCMDprompt += "<br/>"
+        currentCMDprompt += "General Commands<br/>"
         currentCMDprompt += "HELP: print command list <br/>"
-        currentCMDprompt += "CAESAR &#8249;phrase&#8250;: Bruteforce caesar cypher crack a given phrase <br/>"
-        currentCMDprompt += "ECAESAR3 &#8249;phrase&#8250; &#8249;key1&#8250; &#8249;key2&#8250; &#8249;key3&#8250;: Encrypt caesar cypher with 3 rotational keys <br/>"
-        currentCMDprompt += "DCAESAR3 &#8249;phrase&#8250; &#8249;key1&#8250; &#8249;key2&#8250; &#8249;key3&#8250;: Decrypt caesar cypher with 3 rotational keys <br/>"
         currentCMDprompt += "<br/>"
-        currentCMDprompt += "Brute Force Commands<br/>"
-        currentCMDprompt += "BFCAESAR3 &#8249;phrase&#8250;: Bruteforce Caesar cypher with 3 rotational keys, loading results to sysarr<br/>";
+        currentCMDprompt += "Caesar Cypher Commands<br/>"
+        currentCMDprompt += "ECAESAR &#8249;phrase&#8250; &#8249;key1&#8250; &#8249;key2&#8250; &#8249;keyN&#8250;: Caesar cypher encode a given phrase with keys <br/>"
+        currentCMDprompt += "DCAESAR &#8249;phrase&#8250; &#8249;key1&#8250; &#8249;key2&#8250; &#8249;keyN&#8250;: Caesar cypher decode a given phrase with keys <br/>"
+        currentCMDprompt += "BFCAESAR &#8249;phrase&#8250; &#8249;keyCount&#8250;: Bruteforce caesar cypher crack a given phrase. Loads possibilities into sysarr <br/>"
+        currentCMDprompt += "<br/>"
+        currentCMDprompt += "Sysarr Commands<br/>"
         currentCMDprompt += "PREVIEW: previews 10 entries remaining in sysarr<br/>";
         currentCMDprompt += "COUNT: counts entries remaining in sysarr<br/>";
         currentCMDprompt += "FILTER <phrase>: removes all entries with the given phrase<br/>";
         currentCMDprompt += "FREQUENCY <phrase> <count>: removes all entries with the given phrase more than count times<br/>";
         currentCMDprompt += "<br/>"
-    } else if (formattedCommand == "caesar") {
-        if (args.length < 2) {
-            currentCMDprompt += "<span>Error: Missing arg \"Phrase\"</span><br/>"
+    } else if (formattedCommand == "ecaesar") {
+        if (args.length < 3) {
+            currentCMDprompt += "<span>Error: Missing args: Required 2</span><br/>"
         } else {
-            for (let i = 0; i < alphabet.length; i++) {
-                let word = "";
-                for (let j = 0; j < args[1].length; j++) {
-                    let char = args[1].charAt(j).toLowerCase()
-                    if (alphabet.includes(char)) {
-                        word += alphabet[(alphabet.indexOf(char) + i) % alphabet.length]
-                    } else {
-                        word += char
-                    }
-                }
-                currentCMDprompt += word + "<br/>"
+            let keys = []
+            for (let i = 2; i < args.length; i++) {
+                keys.push(Number(args[i]))
             }
+            let word = ecaesar(args[1], keys)
+            currentCMDprompt += word + "<br/>"
         }
-    } else if (formattedCommand == "ecaesar3") {
-        if (args.length < 5) {
-            currentCMDprompt += "<span>Error: Missing args: Required 4</span><br/>"
+    } else if (formattedCommand == "dcaesar") {
+        if (args.length < 3) {
+            currentCMDprompt += "<span>Error: Missing args: Required 2</span><br/>"
         } else {
             let keys = []
             try {
-                keys.push(Number(args[2]))
-                keys.push(Number(args[3]))
-                keys.push(Number(args[4]))
-                let word = ""
-                for (let j = 0; j < args[1].length; j++) {
-                    let char = args[1].charAt(j).toLowerCase()
-                    if (alphabet.includes(char)) {
-                        word += alphabet[(alphabet.indexOf(char) + keys[j % keys.length]) % alphabet.length]
-                    } else {
-                        word += char
-                    }
+                for (let i = 2; i < args.length; i++) {
+                    keys.push(Number(args[i]))
                 }
-                currentCMDprompt += word + "<br/>"
-            } catch {
-                currentCMDprompt += "<span>Error: Cannot parse key numbers.</span><br/>"
-
-            }
-        }
-    } else if (formattedCommand == "dcaesar3") {
-        if (args.length < 5) {
-            currentCMDprompt += "<span>Error: Missing args: Required 4</span><br/>"
-        } else {
-            let keys = []
-            try {
-                keys.push(Number(args[2]))
-                keys.push(Number(args[3]))
-                keys.push(Number(args[4]))
-                let word = ""
-                for (let j = 0; j < args[1].length; j++) {
-                    let char = args[1].charAt(j).toLowerCase()
-                    if (alphabet.includes(char)) {
-                        word += alphabet[(alphabet.indexOf(char) - keys[j % keys.length] + alphabet.length) % alphabet.length]
-                    } else {
-                        word += char
-                    }
-                }
+                let word = dcaesar(args[1], keys)
                 currentCMDprompt += word + "<br/>"
             } catch {
                 currentCMDprompt += "<span>Error: Cannot parse key numbers.</span><br/>"
@@ -142,7 +113,7 @@ function enterCommand(cmd) {
         for (let i = 0; i < sysarr.length && i < 10; i++) {
             currentCMDprompt += sysarr[i] + "<br/>"
         }
-    }else if (formattedCommand == "previewall") {
+    } else if (formattedCommand == "previewall") {
         if (sysarr.length == 0) {
             currentCMDprompt += "No entries in sysarr</br>"
         }
@@ -170,6 +141,17 @@ function enterCommand(cmd) {
         }
 
         currentCMDprompt += "Filter ran. " + removed + " entries removed.<br/>"
+    } else if (formattedCommand == "filterword") {
+        let removed = 0;
+        for (let i = 0; i < sysarr.length; i++) {
+            if (sysarr[i].includes(args[1])) {
+                sysarr.splice(i, 1);
+                removed++;
+                i--;
+            }
+        }
+
+        currentCMDprompt += "Filter ran. " + removed + " entries removed.<br/>"
     } else if (formattedCommand == "frequency") {
         let removed = 0;
         if (args.length < 3) {
@@ -193,27 +175,33 @@ function enterCommand(cmd) {
         }
 
         currentCMDprompt += "Filter ran. " + removed + " entries removed.<br/>"
-    } else if (formattedCommand == "bfcaesar3") {
-        if (args.length < 2) {
-            currentCMDprompt += "<span>Error: Missing args: Required 1</span><br/>"
+    } else if (formattedCommand == "bfcaesar") {
+        if (args.length < 3) {
+            currentCMDprompt += "<span>Error: Missing args: Required 2</span><br/>"
         } else {
+            let keycount = Number(args[2])
             sysarr = [];
-            for (let a = 0; a < alphabet.length; a++) {
-                for (let b = 0; b < alphabet.length; b++) {
-                    for (let c = 0; c < alphabet.length; c++) {
-                        let keys = [a, b, c]
-                        let word = ""
-                        for (let j = 0; j < args[1].length; j++) {
-                            let char = args[1].charAt(j).toLowerCase()
-                            if (alphabet.includes(char)) {
-                                word += alphabet[(alphabet.indexOf(char) + keys[j % keys.length]) % alphabet.length]
-                            } else {
-                                word += char
-                            }
-                        }
-                        sysarr.push(word)
+            for (let keynum = 0; keynum < Math.pow(alphabet.length, keycount); keynum++) {
+                let keys = []
+                for (let i = 0; i < keycount; i++) {
+                    keys.push(0)
+                }
+                keys[0] = keynum;
+                for (let i = 1; i < keycount; i++) {
+                    keys[i] = Math.floor(keys[i - 1] / alphabet.length)
+                    keys[i - 1] = keys[i - 1] % alphabet.length
+                }
+                let word = ""
+                for (let j = 0; j < args[1].length; j++) {
+                    let char = args[1].charAt(j).toLowerCase()
+                    if (alphabet.includes(char)) {
+                        word += alphabet[(alphabet.indexOf(char) + keys[j % keys.length]) % alphabet.length]
+                    } else {
+                        word += char
                     }
                 }
+                sysarr.push(word)
+
             }
         }
         currentCMDprompt += "Loaded.<br/>"
