8add828a67d5451de1b57b61e570f8a285a30909
TJ <TJpnick@gmail.com>
2024-01-23 12:44:10 -0500

    no science smash :/

--- sci_smash/Items/HealthPotion.js
+++
@@ -1,9 +0,0 @@
-import Assets from "../Assets.js";
-import Item from "../Item.js"
-export default class HealthPotion extends Item {
-    constructor() {
-        super();
-        this.text = "+1 health"
-        this.displayImage = Assets.spritesheets.healthpotion;
-    }
-}
--- sci_smash/Items/LifePotion.js
+++
@@ -1,9 +0,0 @@
-import Assets from "../Assets.js";
-import Item from "../Item.js"
-export default class LifePotion extends Item {
-    constructor() {
-        super();
-        this.text = "+1 max health";
-        this.displayImage = Assets.spritesheets.lifepotion;
-    }
-}
