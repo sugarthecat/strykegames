cf69e46353d83539e37ac61ef4aee69ef07aa883
TJ <TJpnick@gmail.com>
2024-02-07 13:52:58 -0500

    Early polygon routing

--- worldbuilding/livemap/script.js
+++ worldbuilding/livemap/script.js
@@ -1,60 +1,230 @@
 let tiles = []
 const NOISE_SCALE = 0.01;
 const TEMP_NOISE_SCALE = 0.008;
-const MAP_WIDTH = 600;
-const MAP_HEIGHT = 400;
-const TILE_WIDTH = 300;
-const TILE_HEIGHT = 200;
-class Tile {
+const LAND_CUTOFF = 0.45;
+const TILE_HEIGHT = 100;
+const TILE_WIDTH = 150;
+const MAP_WIDTH = 1200;
+const MAP_HEIGHT = 800;
+
+class Point {
     constructor(x, y) {
-        this.x = floor(x);
-        this.y = floor(y);
-        this.elevation = noise(this.x * NOISE_SCALE, this.y * NOISE_SCALE) 
-        this.temperature = noise(this.x * TEMP_NOISE_SCALE + 500, this.y * TEMP_NOISE_SCALE + 500)
+        this.x = x;
+        this.y = y;
+    }
+}
+class Tile {
+    constructor() {
+        this.points = []
+        this.points2 = []
+        this.vertexes = []
+        this.color = color(random(10, 255), random(10, 255), random(10, 255))
     }
     Draw() {
-        if (this.elevation > 0.6) {
-            //mountain
-            if(this.temperature < 0.4){
-                fill(200, 200, 250)
-            }else if(this.temperature > 0.6){
-                fill(100,50,0)
-            }else{
-                fill(50, 50, 50)
-            }
-        } else if (this.elevation > 0.45) {
-            //normal land
-            if(this.temperature < 0.4){
-                fill(255,255,255)
-            }else if(this.temperature > 0.6){
-                fill(200,150,0)
-            }else{
-                fill(0,200,0)
-            }
-        } else {
-            fill(0, this.elevation*100, this.elevation*400)
+        for (let i = 0; i < this.points.length; i++) {
+            //vertex(this.points[i].x / TILE_WIDTH * MAP_WIDTH, this.points[i].y / TILE_HEIGHT * MAP_HEIGHT)
+            //circle(this.points[i].x / TILE_WIDTH * MAP_WIDTH, this.points[i].y / TILE_HEIGHT * MAP_HEIGHT, 5)
         }
-        rect((this.x), (this.y), ceil(MAP_WIDTH / TILE_WIDTH)+1, 0.3+ceil(MAP_HEIGHT / TILE_HEIGHT))
+        fill(0)
+        noStroke();
+        for (let i = 0; i < this.points2.length; i++) {
+            circle(this.points2[i].x / TILE_WIDTH * MAP_WIDTH, this.points2[i].y / TILE_HEIGHT * MAP_HEIGHT, 8)
+        }
+        circle(this.firstPoint.x / TILE_WIDTH * MAP_WIDTH, this.firstPoint.y / TILE_HEIGHT * MAP_HEIGHT, 12)
+        fill(this.color)
+        beginShape();
+        for (let i = 0; i < this.vertexes.length; i++) {
+
+            vertex(this.vertexes[i].x / TILE_WIDTH * MAP_WIDTH, this.vertexes[i].y / TILE_HEIGHT * MAP_HEIGHT)
+        }
+        endShape(CLOSE);
+    }
+    Setup() {
+        //remap selected continent
+        let world = [];
+        let mapped = [];
+        //console.log(this.points)
+        for (let i = 0; i < this.points.length; i++) {
+            //console.log(this.points[i].x)
+            while (this.points[i].x >= world.length - 2) {
+                world.push([])
+                mapped.push([])
+            }
+            world[this.points[i].x][this.points[i].y] = true
+            mapped[this.points[i].x][this.points[i].y] = false
+        }
+        for (let i = 0; i < world.length; i++) {
+            for (let j = 0; j < world[i].length; j++) {
+                for (let i2 = -1; i2 <= 1; i2++) {
+                    for (let j2 = -1; j2 <= 1; j2++) {
+                        let newx = i + i2;
+                        let newy = j + j2;
+                        if (
+                            (
+                                (
+                                    newx >= 0
+                                    && (i2 == 0 || j2 == 0)
+                                    && newy >= 0
+                                    && newx < isLandArr.length
+                                    && newy < isLandArr[newx].length
+                                    && world[newx][newy] == undefined
+                                )
+                                ||
+                                (
+                                    newx < 0
+                                    || newy < 0
+                                    || newx >= isLandArr.length
+                                    || newy >= isLandArr[newx].length
+                                )
+                            )
+                            &&
+                            //false ||
+                            world[i][j]
+                        ) {
+                            mapped[i][j] = true;
+                        }
+
+                    }
+                }
+            }
+        }
+        this.points2 = []
+        let firstPoint = null;
+        for (let i = 0; i < world.length; i++) {
+            for (let j = 0; j < world[i].length; j++) {
+                if (mapped[i][j] == true) {
+                    if (firstPoint === null) {
+                        firstPoint = { x: i, y: j }
+                        this.firstPoint = firstPoint;
+                    }
+                    this.points2.push(new Point(i, j))
+                }
+            }
+        }
+        let searchPoint = firstPoint;
+        let going = true;
+        while (going) {
+            going = false;
+            this.vertexes.push(searchPoint);
+            //console.log(points[0])
+            for (let i = -1; i <= 1; i++) {
+                for (let j = -1; j <= 1; j++) {
+                    let newx = i + searchPoint.x
+                    let newy = j + searchPoint.y
+                    if (
+                        newx >= 0
+                        && newy >= 0
+                        && newx < isLandArr.length
+                        && newy < isLandArr[newx].length
+                        && mapped[newx][newy]
+                        && !going
+                    ) {
+                        //console.log(mapped[newx][newy])
+                        searchPoint = ({ x: newx, y: newy })
+                        going = true;
+                    }
+                }
+            }
+            //console.log(consolestr)
+            mapped[searchPoint.x][searchPoint.y] = false;
+        }
     }
 }
 let currDate = new Date();
+
+let isLandArr = []
+let coveredLand = []
 function setup() {
     noStroke();
     randomSeed(currDate.getMonth() + currDate.getFullYear() * 200)
     noiseSeed(currDate.getMonth() + currDate.getFullYear() * 200)
-    createCanvas(MAP_WIDTH, MAP_HEIGHT)
+    createCanvas(1, 1)
+    //generate land array
+    for (let i = 0; i < TILE_WIDTH; i++) {
+        isLandArr.push([])
+        coveredLand.push([])
+        for (let j = 0; j < TILE_HEIGHT; j++) {
+            let x = i * MAP_WIDTH / TILE_WIDTH;
+            let y = j * MAP_HEIGHT / TILE_HEIGHT;
+            let isLand = noise(x * NOISE_SCALE, y * NOISE_SCALE) > LAND_CUTOFF;
+            isLandArr[i].push(isLand)
+            coveredLand[i].push(false);
+            if (isLand) {
+
+                //tiles.push(new Tile(x, y,));
+            }
+        }
+    }
+    //clear islands with < 15 area;
+    for (let i = 0; i < isLandArr.length; i++) {
+        for (let j = 0; j < isLandArr[i].length; j++) {
+            if (isLandArr[i][j] && !coveredLand[i][j]) {
+                let landArea = getLandmass(i, j)
+                //console.log(landArea.length)
+                let newTile = new Tile()
+                for (let i = 0; i < landArea.length; i++) {
+                    newTile.points.push(landArea[i])
+                }
+                newTile.Setup()
+                tiles.push(newTile)
+            }
+        }
+    }
 }
+function getLandmass(x1, y1) {
+    //let landArea = 1;
+    coveredLand[x1][y1] = true;
+    let land = [new Point(x1, y1)]
+    let toSearch = [new Point(x1, y1)]
+    while (toSearch.length > 0) {
+        let x = toSearch[0].x
+        let y = toSearch[0].y
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                let newx = i + x;
+                let newy = j + y
+                if (newx >= 0
+                    && newy >= 0
+                    && (i == 0 || j == 0)
+                    && newx < isLandArr.length
+                    && newy < isLandArr[newx].length
+                    && isLandArr[newx][newy]
+                    && !coveredLand[newx][newy]) {
+                    //landArea += 1
+                    coveredLand[newx][newy] = true;
+                    land.push(new Point(newx, newy))
+                    toSearch.push(new Point(newx, newy))
+                }
+            }
+        }
+        //console.log(`x: ${x}, y: ${y}, len:${toSearch.length}`)
+        toSearch.shift()
+    }
+    return land;
+}
+let camerax = 0;
+let cameray = 0;
 function draw() {
     resizeCanvas(windowWidth, windowHeight);
     scale(max(width / MAP_WIDTH, height / MAP_HEIGHT))
-    for (let i = 0; i < TILE_WIDTH; i++) {
-        tiles.push([])
-        for (let j = 0; j < TILE_HEIGHT; j++) {
-            tiles[i].push(new Tile(i * MAP_WIDTH / TILE_WIDTH, j * MAP_HEIGHT / TILE_HEIGHT));
-            tiles[i][j].Draw();
-        }
+    translate(-camerax, -cameray)
+    background(20, 50, 200)
+    for (let i = 0; i < tiles.length; i++) {
+        tiles[i].Draw();
     }
-
+    let speed = 2;
+    if (keyIsDown(UP_ARROW)) {
+        cameray -= speed;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        cameray += speed;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        camerax -= speed;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        camerax += speed;
+    }
 }
 function mouseClicked() {
 
