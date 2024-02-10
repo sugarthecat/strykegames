bd1e5e42436ed652b87748927901fbdae0f77633
TJ Nickerson <TJPNick@gmail.com>
2024-02-10 16:13:28 -0500

    hopefully news isnt weird

--- worldbuilding/dailynews/data.js
+++ worldbuilding/dailynews/data.js
@@ -1,7 +1,7 @@
-fetch("data/lnames.txt").then(x => x.text()).then(x => x.split(",\r\n")).then(x => last_names = x).then(x => dataLoaded++)
-fetch("data/fnames.txt").then(x => x.text()).then(x => x.split("\r\n")).then(x => first_names = x).then(x => dataLoaded++)
+fetch("data/lnames.txt").then(x => x.text()).then(x => x.split(",\n")).then(x => last_names = x).then(x => dataLoaded++)
+fetch("data/fnames.txt").then(x => x.text()).then(x => x.split("\n")).then(x => first_names = x).then(x => dataLoaded++)
 
-fetch("data/csybs.txt").then(x => x.text()).then(x => x.split("\r\n")).then(x => vowelSyllables = x).then(x => dataLoaded++)
-fetch("data/vsybs.txt").then(x => x.text()).then(x => x.split("\r\n")).then(x => consonantSyllables = x).then(x => dataLoaded++)
+fetch("data/csybs.txt").then(x => x.text()).then(x => x.split("\n")).then(x => vowelSyllables = x).then(x => dataLoaded++)
+fetch("data/vsybs.txt").then(x => x.text()).then(x => x.split("\n")).then(x => consonantSyllables = x).then(x => dataLoaded++)
 let dataLoaded = 0;
 let dataMax = 4;
