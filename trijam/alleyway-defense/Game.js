548f2f47a82b00bc5c8964032ba5d1ebbb7c27e2
TJ Nickerson <TJPNick@gmail.com>
2024-08-04 17:25:28 -0400

    fixed bullet despawn bug

--- trijam/alleyway-defense/Game.js
+++ trijam/alleyway-defense/Game.js
@@ -9,9 +9,9 @@ class GameScreen extends GUI {
     Reset() {
         player = new Player()
         playerBullets = []
-        enemies = [new Enemy(200,2)]
+        enemies = [new Enemy(200, 2)]
         enemyBullets = []
-        cars = [new Car(175,100)]
+        cars = [new Car(175, 100)]
         time_survived = 0
     }
     Draw(x, y) {
@@ -19,80 +19,80 @@ class GameScreen extends GUI {
         timeSinceLastSpawn += deltaTime / 1000
         super.Draw()
         image(Assets.map, 0, 0, 600, 400)
-        for(let i = 0; i<playerBullets.length; i++){
-            if(playerBullets[i].x < -600 || playerBullets[i].x > 600){
-                playerBullets.splice(i,1)
+        for (let i = 0; i < playerBullets.length; i++) {
+            if (playerBullets[i].x < -600 || playerBullets[i].x > 600) {
+                playerBullets.splice(i, 1)
                 i--;
                 continue
-            }else{
+            } else {
 
                 playerBullets[i].Draw()
             }
-            for(let j = 0; j<cars.length; j++){
-                if(cars[j].Collides(playerBullets[i])){
-                    playerBullets.splice(i,1)
+            for (let j = 0; j < cars.length; j++) {
+                if (cars[j].Collides(playerBullets[i])) {
+                    playerBullets.splice(i, 1)
                     i--;
                     break
                 }
             }
         }
-        for(let i = 0; i<enemyBullets.length; i++){
+        for (let i = 0; i < enemyBullets.length; i++) {
             enemyBullets[i].Draw()
             let enemyBullet = enemyBullets[i]
-            if(dist ( enemyBullet.x,enemyBullet.y,player.x,player.y) < 20){
+            if (dist(enemyBullet.x, enemyBullet.y, player.x, player.y) < 20) {
                 screenOn = "fail"
             }
-            
-            if(enemyBullet.x < -800 || enemyBullet.x > 800){
-                enemyBullets.splice(i,1)
+
+            if (enemyBullet.x < -800 || enemyBullet.x > 800) {
+                enemyBullets.splice(i, 1)
                 i--;
                 continue;
             }
-            for(let j = 0; j<cars.length; j++){
-                if(cars[j].Collides(enemyBullets[i])){
-                    enemyBullets.splice(i)
+            for (let j = 0; j < cars.length; j++) {
+                if (cars[j].Collides(enemyBullets[i])) {
+                    enemyBullets.splice(i, 1)
                     i--;
-                    break;
+                    continue;
                 }
             }
         }
-        for(let i = 0; i<enemies.length; i++){
-            if(enemies[i].dead){
-                enemies.splice(i,1)
+        for (let i = 0; i < enemies.length; i++) {
+            if (enemies[i].dead) {
+                enemies.splice(i, 1)
                 i--;
                 continue
-            }else{
+            } else {
 
                 enemies[i].Draw()
             }
         }
         player.Draw()
-        for(let i = 0; i<cars.length; i++){
+        for (let i = 0; i < cars.length; i++) {
             cars[i].Draw()
-            if(cars[i].y > 800 || cars[i].y < -200){
-                cars.splice(i,1)
+            if (cars[i].y > 800 || cars[i].y < -200) {
+                cars.splice(i, 1)
                 i--;
                 continue;
             }
-            if(cars[i].Collides(player)){
+            if (cars[i].Collides(player)) {
                 screenOn = "fail"
             }
         }
         //spawning
-        if(timeSinceLastSpawn > 0.8 + 10 / Math.pow(time_survived,0.7)){
+        if (timeSinceLastSpawn > 0.8 + 10 / Math.pow(time_survived, 0.7)) {
             timeSinceLastSpawn = 0
-            enemies.push(new Enemy(random(50,350),10 / time_survived + 1))
+            enemies.push(new Enemy(random(50, 350), 10 / time_survived + 1))
         }
-        if(cars.length < 1){
+        if (cars.length < 1) {
             let xpos = 175
-            let deltaY = random(80,140)
-            if(random() > 0.5){
+            let deltaY = random(80, 140)
+            if (random() > 0.5) {
                 xpos = 425
                 deltaY *= -1
             }
-            cars.push(new Car(xpos,deltaY))
+            cars.push(new Car(xpos, deltaY))
         }
-        
+
     }
 
     HandleClick(x, y) {
