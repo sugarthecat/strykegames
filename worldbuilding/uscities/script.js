ba592f5cd2f7da6a9bfe6dbc22cafb7462787d0d
TJPNick <TJPNick@gmail.com>
2024-09-11 22:49:55 -0400

    Fixed sc3

--- worldbuilding/uscities/script.js
+++ worldbuilding/uscities/script.js
@@ -12,33 +12,54 @@ function draw() {
     image(mercator, 0, 0, width, height)
     fill(255, 0, 0)
     noStroke()
-    push ()
-    let closestCity = cities[0]
+    push()
+    let selectedCity = cities[0]
     for (let i = 0; i < cities.length; i++) {
         let city = cities[i];
         if (city.population > 1000000 || true) {
-            if (dist(adjX(closestCity.lng), adjY(closestCity.lat), mouseX,mouseY) > dist(adjX(city.lng), adjY(city.lat), mouseX,mouseY)) {
-                closestCity = city
+            if (dist(adjX(selectedCity.lng), adjY(selectedCity.lat), mouseX, mouseY)
+                > dist(adjX(city.lng), adjY(city.lat), mouseX, mouseY)) {
+                selectedCity = city
             }
-            circle(adjX(city.lng), adjY(city.lat), 5)
+            circle(adjX(city.lng), adjY(city.lat), 10)
         }
     }
-    circle(adjX(closestCity.lng),adjY(closestCity.lat),25)
-    fill(0)
-    textSize(height/20)
-    textAlign(CENTER)
-    text (`${closestCity.name} (${closestCity.population.toLocaleString()})`,width/2,100)
+    circle(adjX(selectedCity.lng), adjY(selectedCity.lat), 25)
+    let closestToSelected = cities[0]
+    if (closestToSelected == selectedCity) {
+        closestToSelected = cities[1]
+    }
+    for (let i = 0; i < cities.length; i++) {
+        let city = cities[i];
+        if (city == selectedCity) {
+            continue;
+        }
+        if (adjDist(closestToSelected,selectedCity) > adjDist(city,selectedCity)) {
+            closestToSelected = city
+        }
+    }
+    let borderingCities = [closestToSelected]
+    push ()
+    stroke(255)
+    line(adjX(closestToSelected.lng), adjY(closestToSelected.lat), adjX(selectedCity.lng), adjY(selectedCity.lat))
     pop ()
-    
+    fill(0)
+    textSize(height / 20)
+    textAlign(CENTER)
+    text(`${selectedCity.name} (${selectedCity.population.toLocaleString()})`, width / 2, 100)
+    pop()
 }
-function adjX(x){
+function adjX(x) {
     return 18 * (x + 129.1)
 }
-function adjY(y){
+function adjY(y) {
     return -21 * (y - 56.3)
 }
 async function loadCityData() {
     cities = await (fetch("usCities.json").then(x => x.json()))
     cities = cities.cities
     console.log('loaded')
+}
+function adjDist(city1,city2){
+    return dist(adjX(city1.lng), adjY(city1.lat), adjX(city2.lng), adjY(city2.lat))
 }
