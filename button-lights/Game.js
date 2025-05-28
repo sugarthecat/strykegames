1689854213a035dea4c3f8d168364232394c8332
TJ Nickerson <TJPNick@gmail.com>
2025-05-28 18:13:48 -0400

    Added level 5

--- button-lights/Game.js
+++ button-lights/Game.js
@@ -67,12 +67,11 @@ class GameScreen extends GUI {
             new Button(50, 10, 100, 30, "Levels", function () { screenOn = "levelSelect" }),
             new Button(450, 10, 100, 30, "Reset", function () { ref.NewLevel() })
         ]
+        this.indicators = []
+        this.buttons = []
+        let indicators = this.indicators
         switch (level) {
             case 0:
-                this.buttons = [
-                ]
-                this.indicators = [
-                ]
                 for (let i = 0; i < 10; i++) {
                     let tgtIndicator = new GameIndicator(75 + i * 50, 175, 30)
                     this.indicators.push(tgtIndicator)
@@ -88,7 +87,169 @@ class GameScreen extends GUI {
                     }))
                 }
                 break;
+            case 1:
+                for (let i = 0; i < 4; i++) {
+                    for (let j = 0; j < 2 ** (3 - i); j++) {
+                        let tgtIndicator = new GameIndicator(j * 50 * 2 ** (i) + 100 + 25 * (2 ** i), i * 50 + 150, 30)
+                        this.indicators.push(tgtIndicator)
+                    }
+                }
+                for (let i = 0; i < 8; i++) {
+                    let index = i;
+                    this.buttons.push(new GameButton(i * 50 + 125, 100, 40, function () {
+
+                        let listprog = 0;
+                        for (let i = 0; i < 4; i++) {
+                            if (!indicators[listprog + Math.floor(index / (2 ** i))].active) {
+                                //stuff happens here
+                                if (i == 0) {
+                                    //first row, ez
+                                    indicators[listprog + Math.floor(index / (2 ** i))].active = true
+                                    return
+                                } else {
+                                    if (indicators[listprog - 2 ** (4 - i) + 2 * Math.floor(index / (2 ** i))].active
+                                        && indicators[listprog - 2 ** (4 - i) + 2 * Math.floor(index / (2 ** i)) + 1].active) {
+                                        indicators[listprog + Math.floor(index / (2 ** i))].active = true
+                                    } else {
+                                        let upperBound = listprog + Math.floor(index / (2 ** i))
+                                        let lowerBound = upperBound;
+                                        for (let j = 0; j <= i; j++) {
+                                            for (let k = lowerBound; k <= upperBound; k++) {
+                                                indicators[k].active = false;
+                                            }
+                                            upperBound -= 2 ** (4 + j - i) - Math.floor(index / (2 ** i)) * 2 ** (j) - 2 ** j
+                                            lowerBound -= 2 ** (4 + j - i) - Math.floor(index / (2 ** i)) * 2 ** (j)
+                                        }
+                                    }
+                                }
+                                return;
+                            }
+                            listprog += 2 ** (3 - i)
+                        }
+
+                    }))
+                }
+                break;
+            case 2:
+                addLightsRow(indicators, 300, 175, 6, 40)
+                //password: 231443
+                //btn 1
+                this.buttons.push(new GameButton(225, 225, 40, function () {
+                    if (indicators[1].active && !indicators[2].active) {
+                        indicators[2].active = true
+                    } else {
+                        for (let i = 0; i < 6; i++) {
+                            indicators[i].active = false;
+                        }
+                    }
+                }))
+                //btn 2
+                this.buttons.push(new GameButton(275, 225, 40, function () {
+                    if (!indicators[0].active) {
+                        indicators[0].active = true
+                    } else {
+                        for (let i = 0; i < 6; i++) {
+                            indicators[i].active = false;
+                        }
+                    }
+                }))
+                //btn 3
+                this.buttons.push(new GameButton(325, 225, 40, function () {
+                    if (indicators[0].active && !indicators[1].active) {
+                        indicators[1].active = true
+                    } else if (indicators[4].active && !indicators[5].active) {
+                        indicators[5].active = true
+                    } else {
+                        for (let i = 0; i < 6; i++) {
+                            indicators[i].active = false;
+                        }
+                    }
+                }))
+                this.buttons.push(new GameButton(375, 225, 40, function () {
+                    if (indicators[3].active && !indicators[4].active) {
+                        indicators[4].active = true
+                    } else if (indicators[2].active && !indicators[3].active) {
+                        indicators[3].active = true
+                    } else {
+                        for (let i = 0; i < 6; i++) {
+                            indicators[i].active = false;
+                        }
+                    }
+                }))
+                break;
+            case 3:
+                for (let i = 0; i < 10; i++) {
+                    let index = i;
+                    let tgtIndicator = new GameIndicator(300 + 100 * cos(i * PI * 2 / 10), 200 + 100 * sin(i * PI * 2 / 10), 30)
+                    indicators.push(tgtIndicator)
+                    let buttonFlipper = new GameButton(300 + 150 * cos(i * PI * 2 / 10), 200 + 150 * sin(i * PI * 2 / 10), 40
+                        , function () {
+                            indicators[(index + 9) % 10].Toggle();
+                            indicators[index].Toggle();
+                            indicators[(index + 1) % 10].Toggle();
+                            indicators[10].Toggle();
+                        })
+                    this.buttons.push(buttonFlipper)
+                }
+                indicators.push(new GameIndicator(300, 200, 30))
+                this.buttons.push(new GameButton(50, 200, 40, function () {
+                    indicators[10].Toggle();
+                    indicators[5].Toggle();
+                    indicators[0].Toggle();
+                }))
+                break;
+            case 4:
+                addLightsRow(indicators, 300, 175, 3)
+                addLightsRow(indicators, 300, 225, 5)
+                addLightsRow(indicators, 300, 275, 7)
+                this.buttons.push(new GameButton(300, 125, 40, function () {
+                    //toggle middle row
+                    let ind1 = indicators[1]
+                    let ind2 = indicators[5]
+                    let ind3 = indicators[11]
+                    if (ind1.active == ind2.active) {
+                        ind1.Toggle();
+                        ind2.Toggle();
+                        ind3.active = false;
+                    }
+                }))
+                this.buttons.push(new GameButton(400, 175, 40, function () {
+                    //toggle middle row
+                    let ind1 = indicators[7]
+                    let ind2 = indicators[13]
+                    ind1.Toggle();
+                    ind2.Toggle();
+
+                }))
+                this.buttons.push(new GameButton(100, 225, 40, function () {
+                    //swap forward
+                    let hold = indicators[0].active;
+                    for (let i = 0; i + 1 < 3; i++) {
+                        indicators[i].active = indicators[i + 1].active
+                    }
+                    indicators[2].active = hold
+                    //swap forward - row 2
+                    hold = indicators[3].active;
+                    for (let i = 3; i + 1 < 8; i++) {
+                        indicators[i].active = indicators[i + 1].active
+                    }
+                    indicators[7].active = hold
+                    //swap forward - row 3
+                    hold = indicators[8].active;
+                    for (let i = 8; i + 1 < 15; i++) {
+                        indicators[i].active = indicators[i + 1].active
+                    }
+                    indicators[14].active = hold
+                }))
         }
     }
 
+}
+
+function addLightsRow(indicators, x, y, count, gap = 50) {
+
+    for (let i = 0; i < count; i++) {
+        let tgtIndicator = new GameIndicator(x + gap / 2 - gap / 2 * count + gap * i, y, 30)
+        indicators.push(tgtIndicator)
+    }
 }
--- button-lights/LevelSelect.js
+++ button-lights/LevelSelect.js
@@ -2,10 +2,10 @@ class LevelSelectScreen extends GUI {
     constructor() {
         super();
         this.elements = [new Button(20, 20, 50, 50, "X", function () { screenOn = "title" })]
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 2; i++) {
             for (let j = 0; j < 4; j++) {
                 let levelTo = i * 4 + j
-                this.elements.push(new Button(110 + j * 100, 50 + i * 80, 80, 50, levelTo + 1, function () { 
+                this.elements.push(new Button(110 + j * 100, 130 + i * 80, 80, 50, levelTo + 1, function () { 
                     screenOn = "game"; 
                     level = levelTo; 
                     screens.game.NewLevel()
@@ -15,6 +15,7 @@ class LevelSelectScreen extends GUI {
         this.backgroundColor = color(100)
     }
     DrawBackground() {
+        highestLevel ++;
         //update
         for (let i = 0; i < this.elements.length; i++) {
             this.elements[i].hidden = i > highestLevel+1
