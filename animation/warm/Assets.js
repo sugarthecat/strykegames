3f36a5a41f45439415b81ebcde61d303e7d93313
TJ Nickerson <tjpnick@gmail.com>
2026-08-17 11:09:13 -0400

    Started work on heat

---
+++ animation/warm/Assets.js
@@ -0,0 +1,7 @@
+ class Assets{
+
+    static loadAssets(){
+    }
+    static setVolume(volume){
+    }
+}
---
+++ animation/warm/TitleScreen.js
@@ -0,0 +1,30 @@
+class TitleScreen extends GUI {
+    constructor() {
+        super();
+    }
+}
+
+// he saw the city's glow.
+// like an anvil just struck;
+// like a new place for home, 
+// if he had any luck.
+
+// But the flame at the foundry,
+// is just not the same,
+// as the snug lover's hug
+// that he sold away.
+
+// 
+//
+//
+//
+
+//
+//
+//
+//
+
+// So pick fire in your hearth,
+// or be calm and be free.
+// But we all still want warmth, 
+// and we all just want peace. 
